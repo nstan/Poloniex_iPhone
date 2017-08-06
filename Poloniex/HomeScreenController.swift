8018af095d00aa5226324a5119f8d457160677e4
Nikola Stan <stan.nikola@gmail.com>
2017-08-05 23:56:21 -0600

    works on iphone7 device, not just simulator; Picker view to select currency pair

--- Poloniex/HomeScreenController.swift
+++ Poloniex/HomeScreenController.swift
@@ -13,22 +13,21 @@ import Swamp
 
 var lastRequestTime : DispatchTime = DispatchTime.now()
 let minTimeBetweenRequest:Int = 1
-var s = ""
-var k = ""
 //let tickerRecievedNotificationKey = "nikolastan.com.tickerRecievedNotificationKey"
 
 
-class HomeScreenController: UIViewController, SwampSessionDelegate {
+class HomeScreenController: UIViewController, SwampSessionDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
  
     
-    @IBOutlet weak var publicKeyLabel: UILabel!
-    @IBOutlet weak var secretKeyLabel: UILabel!
+    @IBOutlet weak var currencyPairPickerView: UIPickerView!
+
     @IBOutlet weak var openOrdersLabel: UILabel!
     @IBOutlet weak var currencyPairLabel: UILabel!
     @IBOutlet weak var lastLabel: UILabel!
     
-    var t : Ticker = Ticker(currencyPair: "", last: 0, lowestAsk: 0, highestBid: 0, percentChange: 0, baseVolume: 0, quoteVolume: 0)
+    var tickr : Ticker = Ticker(currencyPair: "", last: 0, lowestAsk: 0, highestBid: 0, percentChange: 0, baseVolume: 0, quoteVolume: 0)
     var currencyPairSetting = "USDT_ETH"
+    var currencyPairList = ["USDT_ETH", "BTC_ETH", "USDT_BTC"]
     
 //    let tickerRecievedNotificationName = Notification.Name(rawValue: tickerRecievedNotificationKey)
 //    
@@ -38,8 +37,15 @@ class HomeScreenController: UIViewController, SwampSessionDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadKeysFromKeychain ()
+        let defaults = UserDefaults.standard
+        let x = defaults.object(forKey: "Currency Pair") as! String?
+        if (x == nil) {defaults.set(currencyPairSetting, forKey: "Currency Pair")} else {currencyPairSetting = x!}
+        
+        currencyPairPickerView.delegate = self
+        currencyPairPickerView.dataSource = self
+        
         updateView ()
+        keys = KeyLoader.loadKeys("publicKey", "secretKey")
         let swampTransport = WebSocketSwampTransport(wsEndpoint:  URL(string: "wss://api.poloniex.com")!)
         let swampSession = SwampSession(realm: "realm1", transport: swampTransport)
         
@@ -71,7 +77,7 @@ class HomeScreenController: UIViewController, SwampSessionDelegate {
         }, onEvent: { details, results, kwResults in
             let cp = results?[0] as! String
             if cp == self.currencyPairSetting {
-                self.t = Ticker(currencyPair: (results?[0] as! String), last: Double(results?[1] as! String)!, lowestAsk: Double(results?[2] as! String)!, highestBid: Double(results?[3] as! String)!, percentChange: Double(results?[4] as! String)!, baseVolume: Double(results?[5] as! String)!, quoteVolume: Double(results?[6] as! String)!)
+                self.tickr = Ticker(currencyPair: (results?[0] as! String), last: Double(results?[1] as! String)!, lowestAsk: Double(results?[2] as! String)!, highestBid: Double(results?[3] as! String)!, percentChange: Double(results?[4] as! String)!, baseVolume: Double(results?[5] as! String)!, quoteVolume: Double(results?[6] as! String)!)
                 self.updateView()
             }
                 // Event data is usually in results, but manually check blabla yadayada
@@ -97,8 +103,31 @@ class HomeScreenController: UIViewController, SwampSessionDelegate {
         
     }
     
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return currencyPairList.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return currencyPairList[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        currencyPairSetting = currencyPairList[row]
+        let defaults = UserDefaults.standard
+        defaults.set(currencyPairSetting, forKey: "Currency Pair")
+//        if .isConnected() {
+//            ss.disconnect()
+//        }
+//        ss.connect()
+    }
+    
+    
     @IBAction func checkOpenOrdersButton(_ sender: UIButton) {
         var response : String = ""
+        
         let (oO, e) = OpenOrdersLoader.returnOpenOrders(currencyPair:"all", keys!)
         for x in oO {
             response = response + String.localizedStringWithFormat("%@: (%@) %.3f at the price of %.3f\n", x.currencyPair!, x.type!, x.amount!, x.rate!)
@@ -113,28 +142,15 @@ class HomeScreenController: UIViewController, SwampSessionDelegate {
     }
     
     
-    
-    
     func updateView () {
-        currencyPairLabel.text = self.t.currencyPair
-        lastLabel.text = String(self.t.last)
-    }
-    
-    func loadKeysFromKeychain () {
-        keys = KeyLoader.loadKeys(
-            "publicKey", "secretKey")
-        guard let x = keys?.key, let y = keys?.secret else {
-            print("error reading keys from the keychain")
-            return
+        if self.tickr.currencyPair == currencyPairSetting {
+              lastLabel.text = String(self.tickr.last)
+        } else {
+            print ("currency pair mismatch")
         }
-        k = x; s = y;
-        if x==nil || y==nil {
-            k = "";
-            s = k;
-        }
-        publicKeyLabel.text = k
-        secretKeyLabel.text = s
+        
+
     }
-    
 }
+
 
